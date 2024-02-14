a5ff48f77d6993505bd758ddbb3cafee9a2d3479
Fatema Tuz Johora <138400099+fatihatuzjohora@users.noreply.github.com>
2024-02-14 19:00:55 +0600

    Add files via upload
    first done

---
+++ module 16/body.js
@@ -0,0 +1,79 @@
+
+
+
+// number 
+var age= 23;
+console.log(age)
+console.log(typeof age)
+// String
+var name='fatiha';
+console.log(name)
+console.log(typeof name)
+
+// boolean---2rokom, 
+var passed=true;
+var marrid=false;
+console.log(passed)
+
+var my_current_home_address= 'DK'//snake case
+var myCurrentHomeAddress= 'DK' //camel case
+var MyCurrentHomeAddress= 'DK' //pascal case
+
+var applePrice =parseInt('20')  //A string to convert into a number.Converts a string to an integer. (Nan full from= not a number)
+console.log(applePrice)
+console.log(typeof applePrice)
+
+var orangePrice =parseFloat('20.5')  //Converts a string to a floating-point number.@param string — A string that contains a floating-point number
+console.log(orangePrice)
+console.log(typeof orangePrice)
+
+
+var first=0.4;
+var secend=0.6;
+var total=first+ secend;
+console.log(total.toFixed(2)) 
+console.log(typeof total)
+//tofixed hocce dosomin ar por kkoiti sonkha bosbe.
+
+const num=50/0;
+
+//----------------------------
+
+
+
+var egg =20;
+var onion=10;
+console.log(egg + onion);
+console.log(egg - onion);
+console.log(egg * onion);
+console.log(egg / onion);
+
+console.log(10/3);
+console.log(10%3); //vagsas koto ata bujar jnno
+
+var price =35;
+price= price+10;
+price +=10; //shortcut lekha
+console.log(price)
+var newPrice=price +30;
+console.log(newPrice);
+var discountedPrice=newPrice-10;
+console.log(discountedPrice);
+
+var fridge=35000;
+fridge +=1000; //shortcut
+console.log(fridge)
+var fridge=35000;
+fridge -=100; //shortcut
+console.log(fridge)
+var fridge=35000;
+fridge *=20; //shortcut
+console.log(fridge)
+var fridge=35000;
+fridge /=1000; //shortcut
+console.log(fridge)
+
+
+
+
+
---
+++ module 16/task.js
@@ -0,0 +1,49 @@
+//task-1
+let take =1000;
+let apple=300;
+let orange=400;
+let appleOrangePrice=apple+orange;
+let returnPrice=take-appleOrangePrice;
+ console.log(returnPrice);
+
+//task-2
+let math=75.25;
+let biology=65;
+let chemistry=80;
+let physics=35.45;
+let bangla=99.50;
+let totalSubjectPoint=math+biology+chemistry+physics+bangla;
+let subjectAverage=parseFloat((totalSubjectPoint/5).toFixed(2));
+ console.log(subjectAverage);
+ // task-3
+
+ let number=119;
+ let divi=5;
+ let remainder=119%5;
+  console.log(remainder);
+ 
+//task-4
+
+let a = isNaN('11');
+console.log(a);
+let b = isNaN('aaaa');
+console.log(b);
+let g = isNaN(11);
+console.log(g);
+var d = isNaN(2-10);
+console.log(d);
+var e = isNaN(10-2);
+console.log(e);
+var f=isNaN("99")
+console.log(f);
+
+
+console.log(20===20); // value+type
+console.log(20=="20"); //value check
+
+
+console.log(10!=="10");// value+type(10 10 ar somon noi, ati akto mittha kotha....ture )
+console.log(10!="10");//value check.(10 10 ar somon noi, ati akto sotto kotha....flase )
+// console.log("aaa"==="bbb");
+let c=isNaN(true);
+// console.log(c); 
